a17cccb26661743f40a2920c8d2beb28c1423e34
Rohit Pradeep Sudarsanan <rohit.psudarsanan@gmail.com>
2023-04-10 22:45:00 +0530

    first push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Ask Me Anything"),
+        ),
+        body: magic(),
+      ),
+    ),
+  );
+}
+
+class magic extends StatefulWidget {
+  @override
+  State<magic> createState() => _magicState();
+}
+
+class _magicState extends State<magic> {
+  int num = 1;
+  void ran() {
+    setState(() {
+      num = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                ran();
+              },
+              child: Image.asset('images/ball$num.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
